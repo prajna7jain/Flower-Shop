23fb8ee72cda703b3b01843c58476dffd494ddb9
prajna7jain <mrprajnajain@gmail.com>
2025-06-11 16:25:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+// Extend interactivity here! Example: Show a pop-up when clicking 'Shop Now'
+document.querySelector("button").addEventListener("click", function() {
+    alert("Redirecting to the shop page!");
+});
