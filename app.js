91f94c7a28b270427bb750a0a83e0f192d79ad7f
prajna7jain <mrprajnajain@gmail.com>
2025-06-12 13:50:03 +0530

    added flower.json file

--- app.js
+++ app.js
@@ -2,3 +2,28 @@
 document.querySelector("button").addEventListener("click", function() {
     alert("Redirecting to the shop page!");
 });
+
+fetch('flower.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('flower-container');
+
+    data.forEach(flower => {
+      const card = document.createElement('div');
+      card.className = 'card';
+
+      card.innerHTML = `
+        <a href="${flower.image_url}" target="_blank">
+          <img src="${flower.image_url}" alt="${flower.name}">
+        </a>
+        <h3>${flower.name}</h3>
+        <p>Type: ${flower.type}</p>
+        <p>Price: ₹${flower.price_in_inr}</p>
+        <p>Image:${image_url}</p>
+      `;
+
+      container.appendChild(card);
+    });
+  })
+  .catch(error => console.error('Error loading JSON:', error));
+
